97df8339793a38cc5f5c756233c982065f805980
K00337062: Kristian Vahtar Ivansek <K00337062@student.tus.ie>
2026-09-22 16:36:15 +0100

    add heading H2

---
+++ script.js
@@ -0,0 +1,45 @@
+let score = 0;
+
+const scoreDisplay = document.getElementById("score");
+const title = document.getElementById("title");
+const attackButton = document.getElementById("attackButton");
+const resetButton = document.getElementById("resetButton");
+const bigAttack = document.getElementById("bigAtkButton");
+
+
+// TODO: create addPoint()
+// TODO: create resetGame()
+// TODO: connect both functions to buttons
+
+
+function addPoint(){
+    if (score < 20){
+    score ++;
+    }
+    scoreDisplay.textContent = score;
+    youWin();
+}
+
+function resetGame() {
+    score = 0;
+    scoreDisplay.textContent = score;
+    title.textContent = "Click the buttons to Attack";
+}
+
+function bigAttackPoint() {
+    if (score < 20){
+    score += 5;
+    }
+    scoreDisplay.textContent = score;
+    youWin();
+}
+
+function youWin(){
+    if (score >= 20){
+        title.textContent = "You Win!";
+    }
+}
+
+attackButton.addEventListener('click', addPoint);
+bigAttack.addEventListener('click', bigAttackPoint);
+resetButton.addEventListener('click', resetGame);
